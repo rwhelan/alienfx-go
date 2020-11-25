48a692d925dffeef55f89f0ed019a519abd450c4
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2020-11-25 11:53:03 -0500

    bug: fix instruction Serialize()

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rwhelan/alienfx-go
 
 go 1.15
 
-require github.com/google/go-cmp v0.5.3
+require (
+	github.com/google/go-cmp v0.5.3
+	github.com/google/gousb v1.1.1
+)
--- pkg/msgtypes/instruction.go
+++ pkg/msgtypes/instruction.go
@@ -27,8 +27,8 @@ func (inst *Instruction) Serialize() []byte {
 
 	b[0] = byte(inst.Type)
 	b[5] = inst.Red
-	b[6] = inst.Blue
-	b[7] = inst.Green
+	b[6] = inst.Green
+	b[7] = inst.Blue
 
 	binary.BigEndian.PutUint16(b[1:3], inst.Duration)
 	binary.BigEndian.PutUint16(b[3:5], inst.Tempo)
